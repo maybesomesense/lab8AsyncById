27aea66b2c7a6407181c50c3d29ef542ffa94878
maybesomesense <228cacka228@mail.ru>
2023-02-25 23:03:09 +0300

    secondCommit

---
+++ src/main/java/com/example/lab1demorest/Entity/result.java
@@ -0,0 +1,15 @@
+package com.example.lab1demorest.Entity;
+
+import java.math.BigInteger;
+
+public class result {
+    BigInteger result;
+
+    public result(BigInteger result){
+        this.result = result;
+    }
+    public BigInteger getResult(){
+        return result;
+    }
+
+}
--- src/main/java/com/example/lab1demorest/controller/ExController.java
+++ src/main/java/com/example/lab1demorest/controller/ExController.java
@@ -1,5 +1,7 @@
 package com.example.lab1demorest.controller;
 
+import com.example.lab1demorest.Entity.result;
+import com.example.lab1demorest.service.fibonacciCount;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,25 +12,14 @@ import java.math.BigInteger;
 @RestController
 @RequestMapping("api/lab")
 public class ExController {
+    private final fibonacciCount service;
+    public ExController(fibonacciCount service) {
+        this.service = service;
+    }
     @GetMapping("/fibonacci")
-    public String fibonacci(@RequestParam BigInteger param1) throws Exception{
-        return "The value at index " + param1 + " has the value " + fibonacciCount.count(param1);
+    public result fibonacci(@RequestParam BigInteger param1){
+        return service.count(param1);
     }
 
-    static class fibonacciCount{
-        // рекурсивный вариант уходит в overflow где-то на 40 индексе, поэтому идеальной реализацией будет через цикл
-        static BigInteger count(BigInteger index){
-            BigInteger OldValue = BigInteger.valueOf(0);
-            BigInteger Value = BigInteger.valueOf(1);
-            BigInteger Hold;
-            if(index.compareTo(BigInteger.valueOf(1)) < 0){return BigInteger.valueOf(0);}
-            for(BigInteger n = BigInteger.valueOf(1); n.compareTo(index) < 0; n = n.add(BigInteger.valueOf(1)))
-            {
-                Hold = Value;
-                Value = Value.add(OldValue);
-                OldValue = Hold;
-            }
-            return Value;
-        }
-    }
+
 }
